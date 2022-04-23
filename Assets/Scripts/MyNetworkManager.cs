ff6b2bd2be334b24bcf1a5549344db554801d786
Hex21189 <hex21189@msn.com>
2022-04-23 20:33:06 +0900

    Initial commit.

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Color randomColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+        MyNetworkPlayer myPlayer = conn.identity.GetComponent<MyNetworkPlayer>();
+        myPlayer.SetDisplayName($"Player {numPlayers}");
+        myPlayer.SetDisplayColor(randomColor);
+    }
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,69 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
+    [SerializeField] private string displayName = "Missing Name";
+
+    [SyncVar(hook = nameof(HandleDisplayColorUpdate))]
+    [SerializeField] private Color displayColor = Color.white;
+
+    #region Server
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetDisplayColor(Color newDisplayColor)
+    {
+        displayColor = newDisplayColor;
+    }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        if (newDisplayName.Length <= 2 || newDisplayName.Length > 20) { return; }
+
+        RpcLogNewName(newDisplayName);
+
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region Client
+
+    private void HandleDisplayNameUpdated(string oldDisplayName, string newDisplayName)
+    {
+        displayNameText.text = newDisplayName;
+    }
+
+    private void HandleDisplayColorUpdate(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    [ContextMenu("Set My Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My new name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+    #endregion
+}
